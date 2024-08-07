92ccdaf78dca3876958eb36eda310901ea905003
nagurbabu <uppalapatinagurbabu@gmail.com>
2024-08-07 23:21:20 +0530

    finding power value

--- matrix_sub_07_08_2024.cpp
+++ matrix_sub_07_08_2024.cpp
@@ -19,18 +19,7 @@ int main(){
         }
     }
 
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            cout<< matrix1[i][j] << " ";
-        }
-        cout<<" ";
-    }
-
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-           cout<< matrix2[i][j] << " ";
-        }
-    }
+    
 
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
