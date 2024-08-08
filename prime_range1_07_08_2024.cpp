3c226c6cfb0d401c3248404fae88224d95a0b961
nagurbabu <uppalapatinagurbabu@gmail.com>
2024-08-08 07:35:24 +0530

    prime using sieve eratothenesis

---
+++ prime_range1_07_08_2024.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n=40;
+    
+    bool is_prime[n+1];
+
+    for(int i=0;i<=n;i++){
+        is_prime[i]=true;
+    }
+
+    is_prime[0]=false;
+    is_prime[1]=false;
+
+    for(int i=2;i<=n;i++){
+        if(is_prime[i]==true){
+            for(int j=i+i;j<=n;j=j+i){
+                is_prime[j]=false;
+            }
+        }
+    }
+
+    for(int i=0;i=n;i++){
+        if(is_prime[i]){
+            cout<<i<" ";
+        }
+    }
+}
