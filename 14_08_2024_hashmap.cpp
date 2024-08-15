f0f9512db4d95a3e7043c214b6ec7ab400359df0
nagurbabu <uppalapatinagurbabu@gmail.com>
2024-08-15 22:01:05 +0530

    hashmap

--- 14_08_2024_hashmap.cpp
+++ 14_08_2024_hashmap.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Human{
+
     public:
         int id;
         string name;
@@ -19,12 +20,12 @@ int main(){
 
     unordered_map<int,string> mp{{1000,"ferrari"},{800,"alto"},{800,"omni"}};
 
-    auto compare=[&mp](const Human& h1,Human& h2){
-        return mp[h1.car]<mp[h2.car];
+    auto compare=[&mp](const Human& h1,const Human& h2){
+        return mp[0]<mp[0];
 
     };
     
-    priority_queue<Human*,vector<Human*>,decltype(compare)>pq(compare);
+    priority_queue<Human&,vector<Human&>,decltype(compare)>pq(compare);
 
     pq.push(new Human(2,"akash","alto"));
     pq.push(new Human(3,"vasanth","omni"));
