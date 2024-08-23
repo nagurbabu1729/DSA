c70366f7fd4c68775136d965d71d7a0bcad808cd
nagurbabu <uppalapatinagurbabu@gmail.com>
2024-08-23 07:34:36 +0530

    div& mul

---
+++ 22_08_2024_div.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter num:";
+    cin>>n;
+    int res=n>>1;
+    cout<<res;
+
+    return 0;
+}
---
+++ 22_08_2024_mul.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter num:";
+    cin>>n;
+    int res=n<<3;
+    
+    cout<<res;
+
+    return 0;
+}
